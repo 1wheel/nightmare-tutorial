ced6c4209bb00f171e5423dbc3e43513b49cf2c8
Adam Pearce <1wheel@gmail.com>
2017-02-10 19:31:45 -0500

    div ids

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const { csvFormat } = require('d3-dsv');
+const d3 = require('d3');
 const Nightmare = require('nightmare');
-const { readFileSync, writeFileSync } = require('fs');
+const {readFileSync, writeFileSync } = require('fs');
 
 const numbers = readFileSync('./tesco-title-numbers.csv', { encoding: 'utf8' }).trim().split('\n')
 
-const START = 'https://eservices.landregistry.gov.uk/wps/portal/Property_Search';
+const START = 'http://ireports.wrapsnet.org/Interactive-Reporting/EnumType/Report?ItemPath=/rpt_WebArrivalsReports/MX%20-%20Arrivals%20by%20Nationality%20and%20Religion';
 
-const getAddress = async id => {
-  console.log(`Now checking ${id}`);
-  const nightmare = new Nightmare({ show: false });
+const getAddress = async date => {
+  var data = '2/9/2017'
+  console.log(`Now checking ${date}`);
+  const nightmare = new Nightmare({ show: true });
 
   // Go to initial start page, navigate to "Detailed enquiry"
   try {
     await nightmare
       .goto(START)
-      .wait('.bodylinkcopy:first-child')
-      .click('.bodylinkcopy:first-child');
+      .wait('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]')
+      .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]', date)
+      .click('input[value="View Report"]');
+      // .wait('.bodylinkcopy:first-child')
+      // .click('.bodylinkcopy:first-child');
   } catch(e) {
     console.error(e);
   }
 
   // On the next page, type the title number into the appropriate box; click submit
   try {
-    await nightmare
-      .wait('input[name="titleNo"]')
-      .type('input[name="titleNo"]', id)
-      .click('input[value="Search »"]');
+    // await nightmare
+    //   .wait('input[name="titleNo"]')
+    //   .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]', id)
+    //   .click('input[value="Search »"]');
   } catch(e) {
     console.error(e);
   }
@@ -38,7 +43,7 @@ const getAddress = async id => {
       })
       .end();
 
-      return { id, address: result[0], lease: result[1] };
+      return { date, address: result[0], lease: result[1] };
   } catch(e) {
     console.error(e);
     return undefined;
