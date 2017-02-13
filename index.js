6fa8f0da23a3f13068f9f8bb8c062fb850e49949
Adam Pearce <1wheel@gmail.com>
2017-02-13 17:27:00 -0500

    works!

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const { csvFormat } = require('d3-dsv');
 const d3 = require('d3');
 const Nightmare = require('nightmare');
 const {readFileSync, writeFileSync } = require('fs');
+const glob = require('glob')
 
-const numbers = readFileSync('./tesco-title-numbers.csv', { encoding: 'utf8' }).trim().split('\n')
+
+var prevDownloaded = glob.sync(__dirname + '/raw/*.csv')
+  .map(d => d.split('raw/')[1].replace('.csv', '').replace(/-/g, '/'))
+
+const dates = d3.timeDay.range(new Date(2002, 0, 1), new Date(), 1)
+  .map(d3.timeFormat('%m/%d/%Y'))
+  .map(d => d.replace(' ', ''))
+  .filter(d => !prevDownloaded.includes(d))
 
 const START = 'http://ireports.wrapsnet.org/Interactive-Reporting/EnumType/Report?ItemPath=/rpt_WebArrivalsReports/MX%20-%20Arrivals%20by%20Nationality%20and%20Religion';
 
 const getAddress = async date => {
-  var date = '2/9/2017'
   console.log(`Now checking ${date}`);
-  const nightmare = new Nightmare({ show: true });
+  const nightmare = new Nightmare({ show: false });
 
-  // Go to initial start page, navigate to "Detailed enquiry"
   try {
-    var reportURL = await nightmare
+    var outData = await nightmare
       .goto(START)
       .wait('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]')
       .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]', date)
@@ -26,24 +32,24 @@ const getAddress = async date => {
       .select('#dnn_ctr513_View_ReportViewer1_ctl04_ctl03_ddValue', 2)
       .select('#dnn_ctr513_View_ReportViewer1_ctl04_ctl13_ddValue', 1)
       .click('#dnn_ctr513_View_ReportViewer1_ctl04_ctl00')
-      // .wait('#dnn_ctr513_View_ReportViewer1_ctl05_ctl04_ctl00_ButtonImgDown')
-      // .click('#dnn_ctr513_View_ReportViewer1_ctl05_ctl04_ctl00_ButtonImgDown')
-      // .click('#dnn_ctr513_View_ReportViewer1_ctl05_ctl04_ctl00_Menu :nth-child(2)')
-      // .click('.bodylinkcopy:first-child');
-      // .wait('#VisibleReportContentdnn_ctr513_View_ReportViewer1_ctl09')
       .wait('#dnn_ctr513_View_ReportViewer1_ctl09 table')
       .evaluate(function(){
         var sel = $find('dnn_ctr513_View_ReportViewer1')
         var internalSel = sel._getInternalViewer()
-        return internalSel.ExportUrlBase + 'CSV'
+        var url = internalSel.ExportUrlBase + 'CSV'
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.overrideMimeType("text/plain; charset=x-user-defined");
+        xhr.send();
+        return xhr.responseText;
 
         // $find('dnn_ctr513_View_ReportViewer1').exportReport('CSV')
       })
-      .then()
+      .end()
 
-    await nightmare.goto('http://ireports.wrapsnet.org' + reportURL)
+    writeFileSync(__dirname + '/raw/' + date.replace(/\//g, '-') + '.csv', outData)
 
-    console.log(reportURL)
   } catch(e) {
     console.error(e);
   }
@@ -75,14 +81,14 @@ const getAddress = async date => {
 
 // getAddress(numbers[0]).then(a => console.dir(a));
 
-const series = numbers.filter((d, i) => !i).reduce(async (queue, number) => {
+const series = dates.reduce(async (queue, number) => {
   const dataArray = await queue;
   dataArray.push(await getAddress(number));
   return dataArray;
 }, Promise.resolve([]));
 
-series.then(data => {
-  const csvData = csvFormat(data.filter(i => i));
-  writeFileSync('./output.csv', csvData, { encoding: 'utf8' })
-})
-.catch(e => console.error(e));
+// series.then(data => {
+//   const csvData = csvFormat(data.filter(i => i));
+//   writeFileSync('./output.csv', csvData, { encoding: 'utf8' })
+// })
+// .catch(e => console.error(e));
