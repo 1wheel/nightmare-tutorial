63de92e56a6b135a6f4da3cf6831a5b20d9cac02
Adam Pearce <1wheel@gmail.com>
2017-02-13 15:29:18 -0500

    closer

--- index.js
+++ index.js
@@ -8,51 +8,74 @@ const numbers = readFileSync('./tesco-title-numbers.csv', { encoding: 'utf8' }).
 const START = 'http://ireports.wrapsnet.org/Interactive-Reporting/EnumType/Report?ItemPath=/rpt_WebArrivalsReports/MX%20-%20Arrivals%20by%20Nationality%20and%20Religion';
 
 const getAddress = async date => {
-  var data = '2/9/2017'
+  var date = '2/9/2017'
   console.log(`Now checking ${date}`);
   const nightmare = new Nightmare({ show: true });
 
   // Go to initial start page, navigate to "Detailed enquiry"
   try {
-    await nightmare
+    var reportURL = await nightmare
       .goto(START)
       .wait('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]')
       .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]', date)
-      .click('input[value="View Report"]');
-      // .wait('.bodylinkcopy:first-child')
+      .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl11$txtValue"]', date)
+      .click('#dnn_ctr513_View_ReportViewer1_ctl04_ctl05_ddDropDownButton')
+      .click('#dnn_ctr513_View_ReportViewer1_ctl04_ctl05_divDropDown_ctl00')
+      .click('#dnn_ctr513_View_ReportViewer1_ctl04_ctl09_ddDropDownButton')
+      .click('#dnn_ctr513_View_ReportViewer1_ctl04_ctl09_divDropDown_ctl00')
+      .select('#dnn_ctr513_View_ReportViewer1_ctl04_ctl03_ddValue', 2)
+      .select('#dnn_ctr513_View_ReportViewer1_ctl04_ctl13_ddValue', 1)
+      .click('#dnn_ctr513_View_ReportViewer1_ctl04_ctl00')
+      // .wait('#dnn_ctr513_View_ReportViewer1_ctl05_ctl04_ctl00_ButtonImgDown')
+      // .click('#dnn_ctr513_View_ReportViewer1_ctl05_ctl04_ctl00_ButtonImgDown')
+      // .click('#dnn_ctr513_View_ReportViewer1_ctl05_ctl04_ctl00_Menu :nth-child(2)')
       // .click('.bodylinkcopy:first-child');
-  } catch(e) {
-    console.error(e);
-  }
+      // .wait('#VisibleReportContentdnn_ctr513_View_ReportViewer1_ctl09')
+      .wait('#dnn_ctr513_View_ReportViewer1_ctl09 table')
+      .evaluate(function(){
+        var sel = $find('dnn_ctr513_View_ReportViewer1')
+        var internalSel = sel._getInternalViewer()
+        return internalSel.ExportUrlBase + 'CSV'
 
-  // On the next page, type the title number into the appropriate box; click submit
-  try {
-    // await nightmare
-    //   .wait('input[name="titleNo"]')
-    //   .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]', id)
-    //   .click('input[value="Search »"]');
-  } catch(e) {
-    console.error(e);
-  }
-
-  try {
-    const result = await nightmare
-      .wait('.w80p')
-      .evaluate(() => {
-        return [...document.querySelectorAll('.w80p')].map(el => el.innerText);
+        // $find('dnn_ctr513_View_ReportViewer1').exportReport('CSV')
       })
-      .end();
+      .then()
 
-      return { date, address: result[0], lease: result[1] };
+    await nightmare.goto('http://ireports.wrapsnet.org' + reportURL)
+
+    console.log(reportURL)
   } catch(e) {
     console.error(e);
-    return undefined;
   }
+
+  // // On the next page, type the title number into the appropriate box; click submit
+  // try {
+  //   // await nightmare
+  //   //   .wait('input[name="titleNo"]')
+  //   //   .type('input[name="dnn$ctr513$View$ReportViewer1$ctl04$ctl07$txtValue"]', id)
+  //   //   .click('input[value="Search »"]');
+  // } catch(e) {
+  //   console.error(e);
+  // }
+
+  // try {
+  //   const result = await nightmare
+  //     .wait('.w80p')
+  //     .evaluate(() => {
+  //       return [...document.querySelectorAll('.w80p')].map(el => el.innerText);
+  //     })
+  //     .end();
+
+  //     return { date, address: result[0], lease: result[1] };
+  // } catch(e) {
+  //   console.error(e);
+  //   return undefined;
+  // }
 };
 
 // getAddress(numbers[0]).then(a => console.dir(a));
 
-const series = numbers.reduce(async (queue, number) => {
+const series = numbers.filter((d, i) => !i).reduce(async (queue, number) => {
   const dataArray = await queue;
   dataArray.push(await getAddress(number));
   return dataArray;
